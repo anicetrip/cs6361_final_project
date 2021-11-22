0b9f9b6856724f030cf14acfb22fca3278dc7926
yin xdryk <1756622125@qq.com>
2021-11-22 19:11:47 +0000

    add KR+JP charactor list

--- KR_JP_characters.py
+++ KR_JP_characters.py
@@ -4,16 +4,23 @@
 :Create:  2021/11/22 19:02
 """
 
-def get_kor_charset(start, end):
+def get_charset(start, end):
     COMPLETE_RANGE = (int(start, 16), int(end, 16))
     COMPLETE_SET = frozenset(chr(code) for code in range(COMPLETE_RANGE[0], COMPLETE_RANGE[1]+1))
     COMPLETE_LIST = sorted(COMPLETE_SET)
     return COMPLETE_LIST
 
-def get_kor_charsetList():
+def get_kor_charset_list():
     start = 'ac00'
     end = 'd7a3'
-    chars = get_kor_charset(start, end)
+    chars = get_charset(start, end)
+    # print(chars)
     return chars
 
+
+def get_jap_charset_list():
+    start = '3040'
+    end = '31FF'
+    chars = get_charset(start, end)
+    print(chars)
 
