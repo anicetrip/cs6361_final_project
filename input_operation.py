d56ce4fe6fa0b90b624e90e8dec83ca93d91b8f3
yin xdryk <1756622125@qq.com>
2021-11-22 17:23:10 +0000

    special operation on numbers

--- input_operation.py
+++ input_operation.py
@@ -14,7 +14,27 @@ def input_operation():
     print("I can judge whether your sentences are  Japanese, Korean, English, Hindī,and Russian!")
     input_sentences = input("input your sentences \n")
     input_sentences = input_sentences.replace("\n", "")
+    input_sentences = stringQ2B(input_sentences)
     input_sentences = input_sentences.rstrip('0123456789１２３４５６７８９０．％')
     punc_CN_EN = '~`!#$%^&*()_+-=|\';":/.,?><~·！@#￥%……&*（）——+-=“：’；、。，？》《{}'
     input_sentences = re.sub(r"[%s]+" %punc_CN_EN, "", input_sentences)
     return input_sentences
+
+
+
+
+def stringQ2B(ustring):
+    """把字符串全角转半角"""
+    return "".join([Q2B(uchar) for uchar in ustring])
+
+
+def Q2B(uchar):
+    """单个字符 全角转半角"""
+    inside_code = ord(uchar)
+    if inside_code == 0x3000:
+        inside_code = 0x0020
+    else:
+        inside_code -= 0xfee0
+    if inside_code < 0x0020 or inside_code > 0x7e: #转完之后不是半角字符返回原来的字符
+        return uchar
+    return chr(inside_code)
